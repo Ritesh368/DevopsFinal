8a5c040a68116338fabaab7813b20aec85b54788
Ritesh368 <126308729+Ritesh368@users.noreply.github.com>
2023-03-01 11:13:49 +0530

    nike

--- Nike-Shoe-Error/Nike-Shoe-Store-master/src/components/Checkout/Checkout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../../App.css";
-import StepperComponent from "./../Stepper/Stepper";
-
-function Checkout() {
-  return <StepperComponent />;
-}
-
-export default Checkout;
--- Nike-Shoe-Error/Nike-Shoe-Store-master/src/store/states/variablesState.js
+++
@@ -1,4 +0,0 @@
-export const variablesState = {
-  totalItems: 0,
-  totalAmount: 0,
-};
